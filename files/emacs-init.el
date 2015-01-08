4c1e66a81a1fca6882c3c7fb2a2bed60be0eedb3
Bakhti Aripov <bakhti@gmail.com>
2015-01-08 19:38:38 +0200

    emacs: bootstrap use-package module

--- files/emacs-init.el
+++ files/emacs-init.el
@@ -1,12 +1,15 @@
 ;; initalize all ELPA packages
 (require 'package)
-(add-to-list 'package-archives
-	     '("melpa-stable" . "http://melpa.org/packages/") t)
-(package-initialize)
 (setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa-stable" . "http://melpa.org/packages/"))
+
+(package-initialize)
 
 ;; Load use-package, used for loading packages
-(require 'use-package)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 ;; keep customize settings in their own file
 (setq custom-file "~/.emacs.d/custom.el")
